dbdb467bde24465f390c283988d919862b811a6a
Rendani-M <makhavhurendani@gmail.com>
2023-12-26 10:11:40 +0200

    payment confirmation p1

--- src/components/payment/PaymentForm2.jsx
+++ src/components/payment/PaymentForm2.jsx
@@ -103,15 +103,15 @@ function PaymentForm2(props) {
   }, []);
 
   useEffect(() => {
-    if (success) {
-      axios.post('http://localhost:4050/confirmation', myData)
-        .then(response => {
-          console.log('Confirmation response:', response.data);
-        })
-        .catch(error => {
-          console.error('Error during confirmation:', error);
-        });
-    }
+    // if (success) {
+    //   axios.post('http://localhost:4050/confirmation', myData)
+    //     .then(response => {
+    //       console.log('Confirmation response:', response.data);
+    //     })
+    //     .catch(error => {
+    //       console.error('Error during confirmation:', error);
+    //     });
+    // }
   }, [success]);
 
   return (
